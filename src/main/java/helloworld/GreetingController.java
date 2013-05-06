29ceeb90a202d3555eaa1eef08945a6ec5a1dc8a
Roman Iuvshin <riuvshin@codenvy.com>
2013-05-06 14:21:43 +0300

    Added some content

---
+++ src/main/java/helloworld/GreetingController.java
@@ -0,0 +1,37 @@
+package helloworld;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GreetingController implements Controller
+{
+
+   @Override
+   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+   {
+      String userName = request.getParameter("user");
+      NewClass newClass = new NewClass();
+      String result = "";
+      if (userName != null)
+      {
+         double bigValuenewMTd = newClass.bigValuenewMTd();
+         result = "Hello, " + userName + "!";
+      }
+
+      ModelAndView view = new ModelAndView("hello_view");
+      view.addObject("greeting", result);
+      return view;
+   }
+
+   public String hjk(HttpServletRequest request, HttpServletResponse response) throws Exception
+   {
+      ModelAndView view2 = new ModelAndView("hello_view2");
+      return view2.getViewName();
+   }
+}
---
+++ src/main/java/helloworld/NewClass.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class NewClass
+{
+   public double bigValuenewMTd()
+   {
+      final double pi = 3.14159265;
+      return pi;
+   }
+}
